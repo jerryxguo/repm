a001a51d0a16d62a6f50baf79d672bfa7281335d
jerryxguo <jerry_xguo@yahoo.com>
2015-04-27 11:37:47 +1000

    backup db  daily

--- process.bat
+++ process.bat
@@ -1,2 +1,6 @@
 cd C:\workspace\django\open\repm\
 python manage.py process_tasks
+set year=%date:~-6,2%
+set month=%date:~3,2%
+set day=%date:~0,2%
+COPY db.sqlite3  db.sqlite3_%year%_%month%_%day%
